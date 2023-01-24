e6fb47e3f5ae0a8b06b158dfe20797c47e83d84a
AppVeyor <ci@appveyor.com>
2023-01-24 13:16:38 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let str = Math.abs(n).toString().split("").reverse().join("");
+  let result;
+
+  for(let i = 0; i < str.length - 1; i++) {
+    if(str.startsWith('0')) {
+        result = +str.slice(1);
+    } else {
+        result = +str;
+    }
+  }
+
+  return result;
 }
